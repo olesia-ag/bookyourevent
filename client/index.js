e93f003a29c8bbab6ff548a37af9df596457f4cd
Olesia Guidi <olgarbuzova@gmail.com>
2020-01-24 16:10:28 -0500

    fixed bug-typo

--- client/index.js
+++ client/index.js
@@ -12,10 +12,11 @@ ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
       <Navbar />
+      {/* <AllVenues /> */}
       <Switch>
         <Route path="/bookform" component={BookForm} />
         <Route path="/singlevenue" component={SingleVenue} />
-        <Route path="/allvenues" components={AllVenues} />
+        <Route path="/allvenues" component={AllVenues} />
         <Route path="/login" component={Login} />
         <Route exact path="/" component={Homepage} />
       </Switch>
